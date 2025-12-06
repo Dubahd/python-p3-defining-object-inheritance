1dacded9d81602ac461c9b50afa8412f8462f0b6
Duba Halakhe <dubahdida@gmail.com>
2025-12-06 22:41:08 +0300

    lab done

--- lib/car.py
+++ lib/car.py
@@ -1,8 +1,8 @@
+# Import the Vehicle class
 from vehicle import Vehicle
 
 class Car(Vehicle):
+    # No need to redefine __init__ - it inherits from Vehicle
+    
     def go(self):
         return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-    
-
-Car.__bases__
